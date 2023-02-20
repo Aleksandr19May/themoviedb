84820612bbe09c6592a64376ebb0b81dadf5c86e
Aleksandr19May <dela737@mail.ru>
2023-02-21 00:26:27 +0400

    y

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class AuthWidget extends StatefulWidget {
   const AuthWidget({super.key});
 
@@ -14,35 +13,48 @@ class _AuthWidgetState extends State<AuthWidget> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Войти в свою учётную запись'),
-        
       ),
       body: ListView(
-        children: const  [ _HeaderWidget()],
+        children: const [_HeaderWidget()],
       ),
     );
   }
 }
-
 
 class _HeaderWidget extends StatelessWidget {
   const _HeaderWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const style =  TextStyle(fontSize: 16,);
+    const style = TextStyle(
+      fontSize: 16,
+    );
     return Padding(
-      padding: const EdgeInsets.only(left:20),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children:  const  [ SizedBox(height: 25,),
+      padding: const EdgeInsets.only(left: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          SizedBox(
+            height: 25,
+          ),
           _FormWidget(),
-            SizedBox(height: 25,),
-            Text('Войти в свою учётную запись',
-           style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold)
-           ),
-            SizedBox(height: 25,),
-           Text('Чтобы пользоваться правкой и возможностями рейтинга TMDB, а также получить персональные рекомендации, необходимо войти в свою учётную запись. Если у вас нет учётной записи, её регистрация является бесплатной и простой. Нажмите здесь, чтобы начать.',style: style),
-            SizedBox(height: 25,),
-           Text('Если Вы зарегистрировались, но не получили письмо для подтверждения, нажмите здесь, чтобы отправить письмо повторно.',style: style),
+          SizedBox(
+            height: 25,
+          ),
+          Text('Войти в свою учётную запись',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+              'Чтобы пользоваться правкой и возможностями рейтинга TMDB, а также получить персональные рекомендации, необходимо войти в свою учётную запись. Если у вас нет учётной записи, её регистрация является бесплатной и простой. Нажмите здесь, чтобы начать.',
+              style: style),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+              'Если Вы зарегистрировались, но не получили письмо для подтверждения, нажмите здесь, чтобы отправить письмо повторно.',
+              style: style),
         ],
       ),
     );
@@ -59,17 +71,65 @@ class _FormWidget extends StatefulWidget {
 class _FormWidgetState extends State<_FormWidget> {
   @override
   Widget build(BuildContext context) {
-    const  textFieldDecoration =  InputDecoration(border: OutlineInputBorder(), contentPadding: EdgeInsets.symmetric(horizontal:10,vertical: 10 ), isCollapsed: true);
-     const  style =  TextStyle(fontSize: 16, color: Color(0xFF212529));
-    return  Column(
+
+
+    const  color =  Color(0xFF01B4E4);
+    const textFieldDecoration = InputDecoration(
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        isCollapsed: true);
+    const style = TextStyle(fontSize: 16, color: Color(0xFF212529));
+    
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        Text('Имя пользователя',style: style,),
-        TextField(
-          decoration: textFieldDecoration,
+      children:  [
+        const Text(
+          'Имя пользователя',
+          style: style,
         ),
-        Text('Пароль',style: style,),
-        TextField( decoration: textFieldDecoration,),
+        const SizedBox(
+          height: 5,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(right:10.0),
+          child: TextField(
+            decoration: textFieldDecoration,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          'Пароль',
+          style: style,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(right:10.0),
+          child: TextField(
+            decoration: textFieldDecoration,
+            obscureText: true,
+          ),
+        ),
+      Row(children: [
+        TextButton(
+          style:const  ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(color),
+            foregroundColor: MaterialStatePropertyAll(Colors.white),
+            padding:MaterialStatePropertyAll(value) ,
+            textStyle: MaterialStatePropertyAll(TextStyle(
+              fontSize: 16,fontWeight: FontWeight.w700
+            ),
+            
+            ) 
+          ),
+          onPressed: () {}, child: const Text('Login')),
+        TextButton(onPressed: () {}, child: const Text('Reset password')),
+      ],)
+      
+      
       ],
     );
   }
